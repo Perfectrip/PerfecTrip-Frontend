76706bc07a26080708d19c7decfe0fa58c6437a0
김윤우 <ynwookim98@gmail.com>
2023-05-22 16:39:04 +0900

    게시판 페이지바 및 검색기능 구현 완료

--- src/components/board/BoardList.vue
+++ src/components/board/BoardList.vue
@@ -5,20 +5,68 @@
         <b-alert show><h3>글목록</h3></b-alert>
       </b-col>
     </b-row>
-    <b-row class="mb-1">
-      <b-col class="text-right">
-        <b-button variant="outline-primary" @click="moveWrite()">글쓰기</b-button>
-      </b-col>
-    </b-row>
-    <b-row>
+
+    <b-container class="bv-example-row mt-3">
+      <b-row class="align-self-center mb-2">
+        <b-col md="2" class="text-start">
+          <b-button variant="outline-primary" size="sm" @click="moveWrite"
+            >글쓰기</b-button
+          >
+        </b-col>
+        <b-col md="7" offset-md="3">
+          <b-form class="d-flex" id="form-search" @submit.prevent="search">
+            <input type="hidden" name="pgno" value="1" />
+            <b-form-select
+              v-model="searchCondition"
+              class="form-select-sm ms-5 me-1 w-50"
+            >
+              <option value="" disabled selected>검색조건</option>
+              <option value="user_id">작성자</option>
+              <option value="title">제목</option>
+              <option value="content">내용</option>
+            </b-form-select>
+            <b-input-group>
+              <b-form-input v-model="searchKeyword" placeholder="검색어..." />
+              <b-button variant="dark" type="submit">검색</b-button>
+            </b-input-group>
+          </b-form>
+        </b-col>
+      </b-row>
+      <!-- 글 목록 테이블 등의 코드 -->
+    </b-container>
+
+    <b-row v-if="articles.length > 0">
       <b-col>
-        <b-table striped hover :items="articles" :fields="fields" @row-clicked="viewArticle">
+        <b-table
+          striped
+          hover
+          :items="articles"
+          :fields="fields"
+          @row-clicked="viewArticle"
+        >
           <template #cell(subject)="data">
-            <router-link :to="{ name: 'boardview', params: { articleNo: data.item.articleNo } }">
+            <router-link
+              :to="{
+                name: 'boardview',
+                params: { articleNo: data.item.articleNo },
+              }"
+            >
               {{ data.item.title }}
             </router-link>
           </template>
         </b-table>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-pagination
+          v-model="pg"
+          :total-rows="totalRows"
+          :per-page="spp"
+          align="center"
+          size="sm"
+          class="mt-3"
+        ></b-pagination>
       </b-col>
     </b-row>
   </b-container>
@@ -39,26 +87,60 @@ export default {
         { key: "registerTime", label: "작성일", tdClass: "tdClass" },
         { key: "hit", label: "조회수", tdClass: "tdClass" },
       ],
+
+      searchCondition: "title",
+      searchKeyword: "",
+      pg: 1,
+      spp: 20,
+      totalRows: 0,
     };
   },
   created() {
-    let param = {
-      pg: 1,
-      spp: 20,
-      key: null,
-      word: null,
-    };
-    listArticle(
-      param,
-      ({ data }) => {
-        this.articles = data;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    this.getSizeOfArticles();
+    this.fetchArticles();
   },
   methods: {
+    getSizeOfArticles() {
+      // 전체 게시글 개수 정보 가지고 오기
+      let param = {
+        pg: this.pg,
+        spp: 1000,
+        key: this.searchCondition,
+        word: this.searchKeyword,
+      };
+      listArticle(
+        param,
+        ({ data }) => {
+          this.totalRows = data.length;
+          console.log("전체 리스트의 개수 : ", this.totalRows);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    fetchArticles() {
+      let param = {
+        pg: this.pg,
+        spp: this.spp,
+        key: this.searchCondition,
+        word: this.searchKeyword,
+      };
+      listArticle(
+        param,
+        ({ data }) => {
+          this.articles = data;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    search() {
+      this.getSizeOfArticles();
+      this.pg = 1;
+      this.fetchArticles();
+    },
     moveWrite() {
       this.$router.push({ name: "boardwrite" });
     },
@@ -67,6 +149,13 @@ export default {
         name: "boardview",
         params: { articleno: article.articleNo },
       });
+    },
+  },
+  watch: {
+    pg() {
+      // pg 값이 변경될 때마다 호출되는 로직 구현
+      // 예: 다음 게시글을 가져오는 API 호출 등
+      this.fetchArticles();
     },
   },
 };
--- src/views/AppBoard.vue
+++ src/views/AppBoard.vue
@@ -1,6 +1,8 @@
 <template>
   <b-container class="bv-example-row mt-3 text-center">
-    <h3 class="underline-hotpink"><b-icon icon="journals"></b-icon> 여행정보공유</h3>
+    <h3 class="underline-hotpink">
+      <b-icon icon="clipboard"></b-icon> 여행정보공유
+    </h3>
     <router-view></router-view>
   </b-container>
 </template>
@@ -12,6 +14,10 @@ export default {
 <style scoped>
 .underline-hotpink {
   display: inline-block;
-  background: linear-gradient(180deg, rgba(255, 255, 255, 0) 70%, rgba(231, 27, 139, 0.3) 30%);
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0) 70%,
+    rgba(231, 27, 139, 0.3) 30%
+  );
 }
 </style>
