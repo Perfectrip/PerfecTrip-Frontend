597acb3d8df464e590d475438ffe3594d4476bb7
iis05251 <iis05251@sookmyung.ac.kr>
2023-05-23 11:45:49 +0900

    맵거리표시

--- src/components/FindKakaoMap.vue
+++ src/components/FindKakaoMap.vue
@@ -88,7 +88,7 @@
                 .then(arg => {
                 // eslint-disable-next-line no-unused-vars
                 let {result_code, summary, sections} = arg?.data?.routes[0];
-                //console.log(arg)
+                console.log(arg)
                 //경로 저장
                 this.route.push(summary.origin.name);
                 summary.waypoints.forEach((way) => {
@@ -101,89 +101,11 @@
                     this.result = this.result+ '-' + String(loc);
                 });
                 this.result = this.result.substring(1);
-                console.log('emit으로 보냄', this.result);
+                //console.log('emit으로 보냄', this.result);
                 this.$emit('way_list', this.result);
-                if(sections[0]){
-                    let {distance, duration, guides : arrays, roads} = sections[0];  //distance : 미터단위, duration : 초 단위
-                    
-                    let detailRoads = [];
-
-                    for(let i=0;i < roads.length;i++){
-                        let arg = roads[i];
-                        let mini = arg.vertexes;
-                        let cursor = 0;
-                        while(cursor < mini.length){
-                            let obj = new window.kakao.maps.LatLng(mini[cursor+1], mini[cursor]);
-                            detailRoads.push(obj);
-                            cursor = cursor + 2;
-                            if(cursor >= 1000000) break;
-                        }                             
-                    }
-                    // eslint-disable-next-line no-unused-vars
-                    arrays = arrays.map( (arg, idx)=>{
-                        let{x, y}  = arg;
-                        if(x && y){
-                            arg.position = new window.kakao.maps.LatLng(arg.y, arg.x);
-                        }
-                        return arg;
-                    });          
-                         
-                    let { title, position} = arrays[0];
-                    // 마커 이미지의 이미지 크기 입니다
-                    let imageSize = new window.kakao.maps.Size(55, 55);
-                    // 마커 이미지를 생성합니다    
-                    let image = new window.kakao.maps.MarkerImage('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/red_b.png', imageSize);
-                    // 마커를 생성합니다
-                    // eslint-disable-next-line no-unused-vars
-                    let marker1 = new window.kakao.maps.Marker({
-                        map: this.map, // 마커를 표시할 지도
-                        position,
-                        title: title? title : '', // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                        image // 마커 이미지 
-                    });
-
-                    let {title : title2, position : position2} = arrays[arrays.length-1];
-                    // 마커 이미지의 이미지 크기 입니다
-                    // 마커 이미지를 생성합니다    
-                    let image2 = new window.kakao.maps.MarkerImage('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/blue_drag.png', imageSize);
-                    // 마커를 생성합니다
-                    // eslint-disable-next-line no-unused-vars
-                    let marker2 = new window.kakao.maps.Marker({
-                        map: this.map, // 마커를 표시할 지도
-                        position : position2,
-                        title: title2? title2 : '', // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                        image : image2 // 마커 이미지 
-                    });                   
-                        
-                    // 지도에 표시할 선을 생성합니다
-                    let polyline = new window.kakao.maps.Polyline({
-                        //path: arrays.map( arg=> arg.position), // 선을 구성하는 좌표배열 입니다
-                        path : detailRoads,
-                        strokeWeight: 5, // 선의 두께 입니다
-                        strokeColor: 'red', // 선의 색깔입니다
-                        strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-                        strokeStyle: 'solid' // 선의 스타일입니다
-                    });
-
-                    // 지도에 선을 표시합니다 
-                    polyline.setMap(this.map);                      
-
-                    let customOverlay = new window.kakao.maps.CustomOverlay({
-                        position: new window.kakao.maps.LatLng(37.39843974939604, 127.10972941510465),
-                        content: `<div class ="label">거리, 시간 : ${distance}, ${duration}</div>`
-                    });
-
-                    // 커스텀 오버레이를 지도에 표시합니다
-                    customOverlay.setMap(this.map);
-
-                    //지우는 방법
-                    // setTimeout(() => {
-                    //     polyline.setMap(null);
-                    //     customOverlay.setMap(null);
-                    //     marker1.setMap(null);
-                    //     marker2.setMap(null);
-                    // }, 3000);
-                }
+                sections.forEach((section) => {
+                  this.makeLine(section);
+                })
             }).catch(err => {
                 console.log(err)
             });
@@ -218,6 +140,101 @@
                     addwindow.open(map, marker);  
               });
       },
+      makeLine(section) {
+                    let {distance, duration, guides : arrays, roads} = section;  //distance : 미터단위, duration : 초 단위
+                    
+                    let detailRoads = [];
+
+                    for(let i=0;i < roads.length;i++){
+                        let arg = roads[i];
+                        let mini = arg.vertexes;
+                        let cursor = 0;
+                        while(cursor < mini.length){
+                            let obj = new window.kakao.maps.LatLng(mini[cursor+1], mini[cursor]);
+                            detailRoads.push(obj);
+                            cursor = cursor + 2;
+                            if(cursor >= 1000000) break;
+                        }                             
+                    }
+                    // eslint-disable-next-line no-unused-vars
+                    arrays = arrays.map( (arg, idx)=>{
+                        let{x, y}  = arg;
+                        if(x && y){
+                            arg.position = new window.kakao.maps.LatLng(arg.y, arg.x);
+                        }
+                        return arg;
+                    });          
+                         
+                    let { title, position} = arrays[0];
+                    // 마커 이미지의 이미지 크기 입니다
+                    let imageSize = new window.kakao.maps.Size(55, 55);
+                    // 마커 이미지를 생성합니다    
+                    let image = new window.kakao.maps.MarkerImage('./flag.png', imageSize);
+                    // 마커를 생성합니다
+                    // eslint-disable-next-line no-unused-vars
+                    let marker1 = new window.kakao.maps.Marker({
+                        map: this.map, // 마커를 표시할 지도
+                        position,
+                        title: title? title : '', // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                        image // 마커 이미지 
+                    });
+
+                    let {title : title2, position : position2} = arrays[arrays.length-1];
+                    // 마커 이미지의 이미지 크기 입니다
+                    // 마커 이미지를 생성합니다    
+                    let image2 = new window.kakao.maps.MarkerImage('./flag.png', imageSize);
+                    // 마커를 생성합니다
+                    // eslint-disable-next-line no-unused-vars
+                    let marker2 = new window.kakao.maps.Marker({
+                        map: this.map, // 마커를 표시할 지도
+                        position : position2,
+                        title: title2? title2 : '', // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                        image : image2 // 마커 이미지 
+                    });                   
+                        
+                    // 지도에 표시할 선을 생성합니다
+                    let polyline = new window.kakao.maps.Polyline({
+                        //path: arrays.map( arg=> arg.position), // 선을 구성하는 좌표배열 입니다
+                        path : detailRoads,
+                        strokeWeight: 5, // 선의 두께 입니다
+                        strokeColor: 'red', // 선의 색깔입니다
+                        strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+                        strokeStyle: 'solid' // 선의 스타일입니다
+                    });
+          console.log(position2);
+                    // 지도에 선을 표시합니다 
+                    polyline.setMap(this.map);    
+                                      
+                    var dis_km = distance / 1000;
+                    // eslint-disable-next-line no-unused-vars
+                    var dur_min = Math.round(duration / 60);
+                    // eslint-disable-next-line no-unused-vars
+                    var dura_str = '';
+                    if (duration < 60) {
+                      dura_str = '<span class="number">' + duration + '</span>초';
+                    }
+                    else if (dur_min < 60) {
+                      dura_str = '<span class="number">' + dur_min + '</span>분';
+                    }
+                    else if (dur_min > 60) {
+                      var hour = Math.floor(dur_min / 60);
+                      var min = dur_min % 60;
+                      dura_str = '<span class="number">' + hour + '</span>시간' + '<span class="number">' + min + '</span>분';
+                    }
+                    let customOverlay = new window.kakao.maps.CustomOverlay({
+                        position: new window.kakao.maps.LatLng(position2.Ma, position2.La),
+                        content: `<row class="overlayWrap" style="height:50px; width:150px; padding:10px; background-color:white; opacity: 70%;">
+                          
+                          <p3 class="accommName" style="font-size:12px;font-weight: 600;text-align:center;color:black;">
+                          거리 : ${dis_km.toFixed(2)}km</p3>
+                          <p3 class="accommName" style="font-size:12px;font-weight: 600;text-align:center;color:black;">
+                          소요 시간 : ${dura_str}</p3>
+                          </row>`
+                    });
+
+                    // 커스텀 오버레이를 지도에 표시합니다
+                    customOverlay.setMap(this.map);
+      }
     },
   };
   </script>
